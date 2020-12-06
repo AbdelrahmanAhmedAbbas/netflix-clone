a25f7c59c8ad0260ced7879e739f4d2c42102d9e
Abdoahmed111 <eng.abdelrahman.ahmed11@gmail.com>
2020-12-06 18:00:29 +0200

    first commit

---
+++ src/global-styles.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+    html, body {
+        font-family: 'Helvitica Neue', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-oxs-font-smoothing: grayscale;
+        background-color: black;
+        color: #333333;
+        font-size: 16;
+    }
+`;
